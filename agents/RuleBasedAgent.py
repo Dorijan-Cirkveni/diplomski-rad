96b895c0001a4c74c19991d80b3fa475fdd5084a
Dorijan Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2024-01-24 22:57:28 +0100

    dodana abstraktna nadklasa

--- agents/RuleBasedAgent.py
+++ agents/RuleBasedAgent.py
@@ -4,21 +4,27 @@ from interfaces import iAgent
 from util import Counter
 
 
-def CallOrEqual(condition,value):
+def CallOrEqual(condition, value):
     if callable(condition):
         return condition(value)
     else:
-        return condition==value
+        return condition == value
 
 
 class iRule:
-    def check(self,data):
+    def check(self, data):
         raise NotImplementedError
+
     def getCategories(self):
         raise NotImplementedError
-    def reduce(self,variable,value):
+
+    def reduce(self, variable, value):
         raise NotImplementedError
+
     def __copy__(self):
+        raise NotImplementedError
+
+    def getResult(self):
         raise NotImplementedError
 
 
@@ -26,6 +32,7 @@ class Rule(iRule):
     def __init__(self, conditions: dict, result):
         self.conditions: dict = conditions
         self.result = result
+
     def getCategories(self):
         return set(self.conditions.keys())
 
@@ -34,46 +41,53 @@ class Rule(iRule):
             if el not in data or data[el] != val:
                 return None
         return self.result
+
     def __copy__(self):
         X = {e: v for e, v in self.conditions}
         new = Rule(X, self.result)
         return new
-    def reduce(self,variable,value):
+
+    def reduce(self, variable, value):
         if variable not in self.conditions:
-            return self,False
-        condition=self.conditions[value]
-        if not CallOrEqual(condition,value):
-            return self,False
+            return False, {}
+        condition = self.conditions[value]
+        if not CallOrEqual(condition, value):
+            return False, {}
         self.conditions.pop(variable)
-        return self,len(self.conditions)==0
+        return len(self.conditions) == 0, {variable}
+
 
 class FirstOrderRule(iRule):
-    def __init__(self,conditions:list,result):
-        self.conditions=conditions
-        self.result=result
+    def __init__(self, conditions: list, result):
+        self.conditions = conditions
+        self.result = result
+
     def __copy__(self):
-        newconds=self.conditions.copy()
-        newres=self.result
-        return FirstOrderRule(newconds,newres)
+        newconds = self.conditions.copy()
+        newres = self.result
+        return FirstOrderRule(newconds, newres)
+
     def getCategories(self):
-        return {e[0] for e in self.conditions}|{"FO"}
-    def reduce(self,variable,value):
-        (categoryCondition,valueCondition)=self.conditions[-1]
-        if CallOrEqual(categoryCondition,variable):
-            if CallOrEqual(valueCondition,value):
+        return {"FO"}
+
+    def reduce(self, variable, value):
+        res = False
+        (categoryCondition, valueCondition) = self.conditions[-1]
+        if CallOrEqual(categoryCondition, variable):
+            if CallOrEqual(valueCondition, value):
                 self.conditions.pop()
-        return self,len(self.conditions)==0
-
-
+        if self.conditions:
+            return False, {}
+        return True, {"FO"}
 
 
 class RuleBasedAgent(iAgent):
-    def __init__(self, rulelist:list, persistent, default):
-        self.rulelist:list = rulelist
+    def __init__(self, rulelist: list, persistent, default):
+        self.rulelist: list = rulelist
         self.byElement = dict()
         for i, rule in enumerate(rulelist):
-            rule:Rule
-            for element, _ in rule.conditions.items():
+            rule: iRule
+            for element, _ in rule.getCategories():
                 D = self.byElement.get(element, set())
                 D.add(i)
                 self.byElement[element] = D
@@ -82,34 +96,42 @@ class RuleBasedAgent(iAgent):
         self.decision = default
 
     def receiveEnvironmentData(self, data: dict):
-        curRules=dict()
-        curByElement=dict()
-        S=set()
-        L=[]
-        for el_main,v in self.byElement.items():
+        curRules = dict()
+        curByElement = dict()
+        S = set()
+        L = []
+        for el_main, v in self.byElement.items():
             if el_main not in data:
                 continue
             S.update(v)
             L.append(el_main)
         for ruleID in S:
             if ruleID not in curRules:
-                rule=self.rulelist[ruleID]
-                curRules[ruleID]=rule.__copy__()
-                for e in rule.conditions:
-                    S=curByElement.get(e,set())
+                rule: iRule = self.rulelist[ruleID]
+                curRules[ruleID] = rule.__copy__()
+                for e in rule.getCategories():
+                    S = curByElement.get(e, set())
                     S.add(ruleID)
-                    curByElement[e]=S
+                    curByElement[e] = S
         while L:
-            el=L.pop()
-            ruleID=
+            el = L.pop()
+            for ruleID in S | self.byElement.get("FO", set()):
+                rule = self.rulelist[ruleID]
+                rule: iRule
+                isSatisfied, remove = rule.reduce(el,data[el])
+                for el in remove:
+                    S:set=self.byElement[el]
+                    S.remove(ruleID)
+                if isSatisfied:
+
         pass
 
     def performAction(self, actions):
-        action=self.default
+        action = self.default
         for e in actions:
-            if self.persistent.get(e,None) in (None,False):
+            if self.persistent.get(e, None) in (None, False):
                 continue
-            action=e
+            action = e
         return action
 
 
@@ -121,9 +143,9 @@ def ruleTest():
     }
     R1 = Rule(X, {"A3": True})
     print(R1.check({'A1': True, 'A2': True, "A4": True}))
-    actions=V2DIRS + [(0, 0)]
-    RBA=RuleBasedAgent([R1],set(V2DIRS),ACTIONS[-1])
-    RBA.receiveEnvironmentData({'A1':True})
+    actions = V2DIRS + [(0, 0)]
+    RBA = RuleBasedAgent([R1], set(V2DIRS), ACTIONS[-1])
+    RBA.receiveEnvironmentData({'A1': True})
 
 
 def main():
