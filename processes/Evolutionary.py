414fa8116a2c178243e20fd3b8b746a8347da811
Dorijan Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2024-01-30 16:03:50 +0100

    Update Evolutionary.py

--- processes/Evolutionary.py
+++ processes/Evolutionary.py
@@ -1,3 +1,4 @@
+import math
 import random
 import interfaces as itf
 
@@ -53,16 +54,17 @@ class iLifeform:
 
 def testAll(population, testSet: list[callable], combinationMethod=lambda L: sum(L)):
     RES = []
+    fin_result = 0
     for unit in population:
         unit: iLifeform
         results = [fn(unit) for fn in testSet]
         fin_result = combinationMethod(results)
-    return RES
+    return RES, fin_result
 
 
 class Selector(itf.iTrainingMethod):
     def __init__(self, lifeformTemplate, agentTemplate,
-                 populationSize=100, elitism=0.5, birthrate=2, cRate=0.1, mRate=0.1, randomSeed=None):
+                 populationSize=100, elitism=0.5, birthrate=2, cRate=0.1, mRate=0.1, combinationMethod=lambda L: sum(L), randomSeed=None):
         super().__init__(agentTemplate)
         self.randomizer = random.Random(randomSeed) if randomSeed is not None else random.Random
         self.population: list = []
@@ -71,24 +73,52 @@ class Selector(itf.iTrainingMethod):
         self.populationSize = populationSize
         self.elitism = int(populationSize * elitism) if elitism < 1 else elitism
         self.birthrate = birthrate
+        self.cRate = cRate
+        self.mRate = mRate
+        self.combinationMethod = combinationMethod
 
-    def initiate(self, trainingSet:list[callable], evalSet: list[callable], randomSeed=None):
+    def testAll(self,population, testSet: list[callable]):
+        RES = []
+        fin_result = 0
+        for unit in population:
+            unit: iLifeform
+            results = [fn(unit) for fn in testSet]
+            fin_result = self.combinationMethod(results)
+        return RES, fin_result
+
+    def initiate(self, trainingSet: list[callable], randomSeed=None):
         self.population = []
-        units = [self.template.copy() for i in range(self.populationSize)]
+        units = [self.template.copy() for _ in range(self.populationSize)]
         for unit in units:
-            unit.setRandomStats()
-        results = testAll(units)
+            unit.setRandomStats(randomSeed)
+        results, total = testAll(units, trainingSet)
+        eval_results, eval_total = testAll(units, trainingSet)
         while units:
-            self.population.append((units.pop(), results.pop()))
-        return
+            self.population.append((units.pop(), results.pop(), eval_results.pop()))
+        self.population.sort(key=lambda E:E[1],reverse=True)
+        return total, eval_total
 
-    def runGeneration(self):
+    def runGeneration(self, trainingSet: list[callable], evalSet: list[callable], randomSeed=None):
         self.population: list
-        X: list[iLifeform] = self.randomizer.sample(self.population, self.populationSize)
-        for i in range(0, len(X) - 1, 2):
-            unit: iLifeform = X[i].makeNew(X[i + 1], randomSeed=self.randomizer.random())
-            score = self.test(unit)
-            self.population.append((unit, score))
+        units = []
+        for i in range((self.populationSize-self.elitism)*self.birthrate):
+            a=self.randomizer.randint(0,self.populationSize-1)
+            b=self.randomizer.randint(0,self.populationSize-2)
+            if b>=a:
+                b+=1
+            unit: iLifeform = self.population[a].makeNew(self.population[b], cRate=self.cRate, mRate=self.mRate, randomSeed=randomSeed)
+            units.append(unit)
+        results, new_total = testAll(units, trainingSet)
+        eval_results, new_eval_total = testAll(units, evalSet)
+        newadditions=[]
+        while units:
+            newadditions.append((units.pop(), results.pop(), eval_results.pop()))
+        newadditions.sort(key=lambda E:E[1],reverse=True)
+        newgeneration=self.population[:self.elitism]+newadditions
+        self.population=newgeneration[:self.populationSize]
+        self.population.sort()
+        return 
+
 
 
 def main():
