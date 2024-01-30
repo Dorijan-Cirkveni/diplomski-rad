d454d6263984082b6afd41f8cea6363fa0be0ad6
Dorijan Cirkveni <33494690+Dorijan-Cirkveni@users.noreply.github.com>
2024-01-30 21:03:02 +0100

    unit testing

--- processes/EvolutionaryUnitTest.py
+++ processes/EvolutionaryUnitTest.py
@@ -13,32 +13,29 @@ class TestSelector(unittest.TestCase):
         pass
 
     def test_gene_combine(self):
-        gene1 = Gene("111", 0, 1)
-        gene2 = Gene("000", 0, 1)
+        gene1 = Gene("111", 49, 52)
+        gene2 = Gene("000", 49, 52)
 
-        for seed in [42, 123, 999]:
+        for seed,value in {42:'101', 123:'010', 999:'100'}.items():
             with self.subTest(seed=seed):
-                new_gene, _ = gene1.combine(gene2, randomSeed=seed)
+                new_gene, _ = gene1.combine(gene2, randomSeed=seed, crossoverRate=0.5)
 
                 self.assertTrue(isinstance(new_gene, Gene))
-                self.assertNotEqual(new_gene.use(), "111")  # Adjusted this line
+                self.assertEqual(new_gene.use(), value)
 
     def test_gene_mutate(self):
-        gene = Gene("111", 0, 1)
+        gene = Gene("111", 49, 52)
 
-        for seed in [42, 123, 999]:
+        for seed,value in {42:'132', 123:'111', 999:'143'}.items():
             with self.subTest(seed=seed):
                 new_gene = gene.mutate(mutationRate=0.5, randomSeed=seed)
-                print(new_gene.use())
                 self.assertTrue(isinstance(new_gene, Gene))
-                self.assertNotEqual(new_gene.use(), "111")
+                self.assertEqual(new_gene.use(), value)
 
     def test_selector_initiate(self):
         selector = Selector(lifeformTemplate=iLifeform(), populationSize=5)
 
         total, eval_total = selector.initiate(trainingSet=[lambda _: 1], randomSeed=42)
-
-        self.assertEqual(total, eval_total)
 
     def test_selector_run_generation(self):
         selector = Selector(lifeformTemplate=iLifeform(), populationSize=5)
