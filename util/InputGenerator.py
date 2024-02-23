dad8909bb000210fd6adb198839914ed1e89dc43
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-02-23 18:45:58 +0100

    Update InputGenerator.py

--- util/InputGenerator.py
+++ util/InputGenerator.py
@@ -9,13 +9,18 @@ class iSplittableInputGroup:
     def generateRandom(self, randomizer: random.Random):
         raise NotImplementedError
 
+
 class InputGrid(iSplittableInputGroup):
-    def __init__(self,start:tuple,end:tuple):
-        trueMin,trueMax=TDO.Tmin(start,end),TDO.Tmax(start,end)
-        self.start=trueMin
-        self.end=trueMax
+    def __init__(self, start: tuple, end: tuple):
+        trueMin, trueMax = TDO.Tmin(start, end), TDO.Tmax(start, end)
+        self.start = trueMin
+        self.end = trueMax
+
     def splitByRatio(self, ratio: list[int], specialRequests: dict) -> list:
-        
+        return []
+
+    def generateRandom(self, randomizer: random.Random):
+        return
 
 
 def main():
