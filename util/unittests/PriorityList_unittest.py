0f153e9eb975ac775661386ebe00f90be6e370ee
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-03-17 15:18:44 +0100

    unit test done

---
+++ util/unittests/PriorityList_unittest.py
@@ -0,0 +1,20 @@
+import unittest
+from util.PriorityList import PriorityList
+
+class TestPriorityList(unittest.TestCase):
+    def test_add(self):
+        plist = PriorityList()
+        plist.add(3, 'value1')
+        plist.add(5, 'value2')
+        plist.add(3, 'value3')
+        self.assertEqual(plist.allValues, {3: ['value1', 'value3'], 5: ['value2']})
+
+    def test_popLowerThan(self):
+        plist = PriorityList()
+        plist.add(3, 'value1')
+        plist.add(5, 'value2')
+        plist.add(3, 'value3')
+        self.assertEqual(plist.popLowerThan(4), [(3, ['value1', 'value3'])])
+
+if __name__ == '__main__':
+    unittest.main()
