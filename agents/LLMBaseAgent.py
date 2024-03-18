07bbf61568d8cefb4a70a2cb4d64a0dae7e7cfed
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-03-18 09:57:03 +0100

    Implemented model loading and saving

--- agents/LLMBaseAgent.py
+++ agents/LLMBaseAgent.py
@@ -1,3 +1,5 @@
+import os.path
+
 import torch
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
@@ -5,15 +7,29 @@ from interfaces import iAgent
 
 
 class GPTAgent(iAgent):
-    def __init__(self,saveFile:str="",agentFile="",source="gpt2"):
-        self.saveFile:str=saveFile
-        self.agentFile:str=agentFile
-        self.source:str=source
-        self.tokenizer=None
-        self.model=None
-        if self.saveFile:
-
+    def __init__(self, saveFile: str = "", agentFile="", source="gpt2"):
+        self.saveFile: str = saveFile
+        self.agentFile: str = agentFile
+        self.source: str = source
+        self.tokenizer = None
+        self.model = None
+        if not (saveFile and self.load_model_from(saveFile)):
+            self.load_model_from(source)
+        if saveFile:
+            self.save_model_to(saveFile)
         return
+
+    def load_model_from(self, origin: str):
+        if not os.path.exists(origin):
+            return False
+        self.tokenizer = GPT2Tokenizer.from_pretrained(origin)
+        self.model = GPT2LMHeadModel.from_pretrained(origin)
+        return True
+
+    def save_model_to(self, origin):
+        self.model.save_pretrained(origin)
+        self.tokenizer.save_pretrained(origin)
+
     def receiveEnvironmentData(self, data):
         raise NotImplementedError
 
@@ -25,7 +41,6 @@ class GPTAgent(iAgent):
 
     def __copy__(self):
         raise NotImplementedError
-
 
 
 # Load pre-trained GPT-2 model and tokenizer
@@ -47,6 +62,7 @@ output = model.generate(input_ids, max_length=50, num_return_sequences=1)
 # Decode and print output
 generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
 print(generated_text)
+
 
 def main():
     return
