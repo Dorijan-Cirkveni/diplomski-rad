4fd559756ac6fc67633059b920e46ecb0f5b0498
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-03-18 10:28:43 +0100

    set up LLM agent base

--- agents/LLMBaseAgent.py
+++ agents/LLMBaseAgent.py
@@ -6,29 +6,38 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel
 from interfaces import iAgent
 
 
-class GPTAgent(iAgent):
+class BaseGPTAgent(iAgent):
     def __init__(self, saveFile: str = "", agentFile="", source="gpt2"):
         self.saveFile: str = saveFile
         self.agentFile: str = agentFile
         self.source: str = source
         self.tokenizer = None
         self.model = None
-        if not (saveFile and self.load_model_from(saveFile)):
-            self.load_model_from(source)
         if saveFile:
-            self.save_model_to(saveFile)
+            self.LoadFromLocal(saveFile)
+        if self.model is None:
+            self.model = GPT2LMHeadModel.from_pretrained(source)
+            if saveFile:
+                self.model.save_pretrained(saveFile)
+        if self.tokenizer is None:
+            self.tokenizer = GPT2Tokenizer.from_pretrained(source)
+            if saveFile:
+                self.tokenizer.save_pretrained(saveFile)
         return
 
-    def load_model_from(self, origin: str):
+    def LoadFromLocal(self, origin: str):
         if not os.path.exists(origin):
             return False
+
+        tokenizer_path = os.path.join(origin, "tokenizer_config.json")
+        model_path = os.path.join(origin, "pytorch_model.bin")
+
+        if not (os.path.exists(tokenizer_path) and os.path.exists(model_path)):
+            return False
+
         self.tokenizer = GPT2Tokenizer.from_pretrained(origin)
         self.model = GPT2LMHeadModel.from_pretrained(origin)
         return True
-
-    def save_model_to(self, origin):
-        self.model.save_pretrained(origin)
-        self.tokenizer.save_pretrained(origin)
 
     def receiveEnvironmentData(self, data):
         raise NotImplementedError
@@ -44,23 +53,21 @@ class GPTAgent(iAgent):
 
 
 # Load pre-trained GPT-2 model and tokenizer
-print("Importing tokenizer...")
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-print("Importing model...")
-model = GPT2LMHeadModel.from_pretrained("gpt2")
+print("Importing...")
+agent=BaseGPTAgent("temp","gpt2")
 print("Done!")
 # Define input text
 input_text = input()
 input_text = "Hello, how are you?" if not input_text else input_text
 
 # Tokenize input text
-input_ids = tokenizer.encode(input_text, return_tensors="pt")
+input_ids = agent.tokenizer.encode(input_text, return_tensors="pt")
 
 # Generate output sequence
-output = model.generate(input_ids, max_length=50, num_return_sequences=1)
+output = agent.model.generate(input_ids, max_length=50, num_return_sequences=1)
 
 # Decode and print output
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+generated_text = agent.tokenizer.decode(output[0], skip_special_tokens=True)
 print(generated_text)
 
 
