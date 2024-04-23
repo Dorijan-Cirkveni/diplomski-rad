9f155a4f9bb9931f6a52748501a75401d8255c68
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-04-23 18:25:30 +0200

    maze stuff

--- environments/MazeEnvironment.py
+++ environments/MazeEnvironment.py
@@ -33,7 +33,7 @@ def rect(E, grid):
 
 
 mazeCreators = {
-
+    ""
 }
 
 
--- util/mazes.py
+++ util/mazes.py
@@ -6,31 +6,51 @@ from util.Grid2D import Grid2D
 from util.TupleDotOperations import *
 
 
+class iMazeCreator:
+    def create_maze(self, scale: tuple, start: tuple, rand: random.Random):
+        raise NotImplementedError
 
-def even_maze_DFS(true_size:tuple,start:tuple,offset:tuple,rand:random.Random):
-    even_size=tuple([e*2+1 for e in true_size])
-    grid=Grid2D(even_size)
-    even_start=Tmul(start,(2,2))
-    grid[even_start]=1
-    L=[even_start]
-    while L:
-        cur=L.pop()
-        X=grid.get_neighbours(cur)
-        Y=[]
-        for E in X:
-            E2=Toper(cur,E,lambda A,B:B*2-A)
-            if grid[E2]==1:
+
+class EvenMazeCreatorDFS(iMazeCreator):
+    def create_layout(self, scale: tuple, start: tuple, rand: random.Random):
+        grid: Grid2D = Grid2D(scale)
+        grid[start] = 1
+        L = [start]
+        leaves = set()
+        while L:
+            cur = L[-1]
+            X = grid.get_neighbours(cur)
+            print(cur, X)
+            Y = []
+            for E in X:
+                E2 = Toper(cur, E, lambda A, B: B * 2 - A)
+                if grid[E2] == 1:
+                    continue
+                Y.append((E, E2))
+            if not Y:
+                leaves.add(L.pop())
                 continue
-            Y.append((E,E2))
-        E,E2=rand.choice(Y)
-        grid[E]=1
-        grid[E2]=1
-    return grid
+            E, E2 = rand.choice(Y)
+            grid[E] = 1
+            grid[E2] = 1
+            L.append(E2)
+        return grid, leaves
 
-def select_goal():
+    def create_maze(self, scale: tuple, start: tuple, rand: random.Random, tiles: tuple = (2, 0, 1)):
+        grid: Grid2D
+        leaves: set
+        grid, leaves = self.create_layout(scale, start, rand)
+        L = list(leaves)
+        L.sort()
+        goal = rand.choice(L)
+        grid[goal] = 2
+        grid.apply(lambda e: tiles[e])
+        return grid
 
 
-
+mazeMethods = {
+    "even_DFS": EvenMazeCreatorDFS()
+}
 
 
 def bestPossibleScore(grid: Grid2D, start: tuple, goal: tuple, passables=None, mark=None):
@@ -52,38 +72,18 @@ def bestPossibleScore(grid: Grid2D, start: tuple, goal: tuple, passables=None, m
     return -1
 
 
-def generate_test(mazeCreator:iMazeCreator, seed: int, showcrit: callable):
+def generate_test(mazeCreator: iMazeCreator, seed: int):
     rand = random.Random(seed)
-    size = (10, 10)
-    start = Trandom(size, seeder=rand)
-    end = Trandom(size, seeder=rand)
-    res: Grid2D = mazeCreator.generate_maze(rand)
-    return size, start, end, res
+    size = (15, 15)
+    start = tuple([rand.randrange(0, size[i], 2) for i in range(2)])
+    res: Grid2D = mazeCreator.create_maze(size, start, rand)
+    return size, start, res
 
 
 def main():
-    fullscore = 0
-    rand = random.Random(42)
-    data=((25,25),(12,12))
-    A=iMazeCreator(*data)
-    B=DFSMazeCreator(*data)
-    C=A.__copy__()
-    D=B.__copy__()
-    for E in (A,B,C,D):
-        print(type(E))
-
-    def doShow(*args):
-        return args[-1] == -1
-
-    for i in range(0*1000):
-        seed = rand.randint(0, 1000)
-        size, start, end, res = generate_test(seed, doShow)
-        if bestPossibleScore(res, start, end) == -1:
-            print(i, seed, size, start, end, res.unique_values())
-            print(res.text_display("01234"))
-            score = bestPossibleScore(res, start, end)
-            print(score)
-            input("Press Enter")
+    A, B, C = generate_test(EvenMazeCreatorDFS(), 42)
+    print(A, B)
+    print(C.text_display("012345"))
     return
 
 
