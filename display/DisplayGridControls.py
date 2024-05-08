071d98f1323194ada997b1778c958bce39bc5c12
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-08 22:24:09 +0200

    Got the inputs to work

--- display/DisplayGridControls.py
+++ display/DisplayGridControls.py
@@ -32,10 +32,26 @@ class GridConsole(tk.Frame):
             button.grid(row=y, column=x)
             self.buttons[direction] = button
 
+class MainGridControls(tk.Frame):
+    def __init__(self, master=None, returnFunction: callable = None):
+        super().__init__(master)
+        choices = ['GB', 'MB', 'KB']
+        variable = tk.StringVar(master)
+        variable.set('GB')
+
+        w = tk.OptionMenu(master, variable, *choices)
+        w.pack()
+        console=GridConsole(self,self.passer)
+        console.pack()
+
+    def passer(self,E):
+        print("Passed "+str(E))
+        return
+
 
 def main():
     root = tk.Tk()
-    app = GridConsole(master=root)
+    app=MainGridControls(root)
     app.pack()
     root.mainloop()
 
