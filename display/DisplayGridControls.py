7fe7ec3b8b8861c5ca60d4e48c992216660a29f6
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-08 20:38:47 +0200

    Return function working

--- display/DisplayGridControls.py
+++ display/DisplayGridControls.py
@@ -1,5 +1,43 @@
+import tkinter as tk
+
+
+class GridConsole(tk.Frame):
+    def __init__(self, master=None, returnFunction: callable = None):
+        super().__init__(master)
+        self.master = master
+        self.buttons = {}
+        self.returnFunction = lambda E: print(E)
+        if returnFunction:
+            self.returnFunction = returnFunction
+        self.create_widgets()
+
+    def factory(self,E):
+        def func():
+            return self.returnFunction(E)
+        return func
+
+    def create_widgets(self):
+        # Create the buttons
+        X = {
+            (0, 1): "Up",
+            (1, 0): "Left",
+            (1, 1): "Wait",
+            (1, 2): "Right",
+            (2, 1): "Down"
+        }
+        self.buttons = {}
+        for (y, x), txt in X.items():
+            direction=(y - 1, x - 1)
+            button = tk.Button(self, text=txt, command=self.factory(direction))
+            button.grid(row=y, column=x)
+            self.buttons[direction] = button
+
+
 def main():
-    return
+    root = tk.Tk()
+    app = GridConsole(master=root)
+    app.pack()
+    root.mainloop()
 
 
 if __name__ == "__main__":
