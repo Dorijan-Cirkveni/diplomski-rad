ba248efb3a7686dd9a84205ee91b651d3afacc95
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-14 15:35:26 +0200

    Update CyclingGoalEnvironment.py

--- environments/CyclingGoalEnvironment.py
+++ environments/CyclingGoalEnvironment.py
@@ -4,13 +4,45 @@ from util.struct.baseClasses import *
 
 
 class CycleGridRoutine(GridRoutine):
-    def __init__(self, grid: G2Dlib.Grid2D):
+    """
+    A grid routine made of rotating square/rectangle layers nested like this:
+    11111
+    12221
+    12321
+    12221
+    11111
+    """
+    def __init__(self, grid: G2Dlib.Grid2D, speeds: list[int]):
+        """
+        Initialize the CycleGridRoutine.
+
+        :param grid: The grid state for t=0.
+        :param speeds: Number of steps and direction each layer takes (starting from the inside going in).
+                       +=clockwise, -=counterclockwise.
+        """
         super().__init__(grid)
-        return
+        self.speeds = speeds
 
     @staticmethod
     def from_string(s):
+        """
+        Create a CycleGridRoutine from a string representation.
+
+        :param s: String representation of the CycleGridRoutine.
+
+        :raises ImplementAsNeededException: This method doesn't need to be implemented.
+        """
         raise util.CommonExceptions.ImplementAsNeededException()
+
+
+    def getCurGrid(self, itid):
+        """
+        Get grid for current iteration.
+        :param itid: Current iteration number.
+        :return: Corresponding grid.
+        """
+        # Return grid state in iteration
+        return grid
 
 class CyclingGoalEnvironment(GridEnvironment):
     def __init__(self, size:int, rounds:list[list[int]], entities: list[GridEntity], activeEntities: set,
