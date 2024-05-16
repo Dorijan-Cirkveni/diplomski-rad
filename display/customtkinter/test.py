45aaf0fc20f8c65619887283be2bc2f5f7f9ad40
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-16 23:27:53 +0200

    good night

---
+++ display/customtkinter/test.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import customtkinter as ctk
+
+class MainCTKFrame:
+    def __init__(self,root=ctk.):
+        self.root=C
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
