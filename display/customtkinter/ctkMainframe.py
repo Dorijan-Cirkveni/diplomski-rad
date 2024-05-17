3e58376362dbb18cb0504d492b483d6acbc4b4ea
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-17 19:18:43 +0200

    Moved mainframe

--- display/customtkinter/ctkMainframe.py
+++ display/customtkinter/ctkMainframe.py
@@ -5,29 +5,26 @@ import customtkinter as ctk
 from ctkScrollableFrames import ScrollableFrameBase, CategoricalScrollableFrame
 
 
-class MainCTKFrame:
-    def __init__(self, root: ctk.CTk, dimensions: tuple[int, int]):
-        self.root = root
-        self.root.geometry("{}x{}".format(*dimensions))
-        self.root.title("CustomTkinter Scrollable Frames Example")
+class MainCTKFrame(ctk.CTkFrame):
+    def __init__(self, master:ctk.CTk, dimensions: tuple[int, int], **kwargs):
+        super().__init__(master, **kwargs)
+        self.master:ctk.CTk
+        self.master.geometry("{}x{}".format(*dimensions))
+        self.master.title("CustomTkinter Scrollable Frames Example")
 
     def create_widgets(self):
-        """
-
-        :return:
-        """
-        self.root.grid_columnconfigure(0, weight=1)
-        self.root.grid_columnconfigure(1, weight=1)
-        self.root.grid_columnconfigure(2, weight=1)
-        self.root.grid_rowconfigure(0, weight=1)
+        self.master.grid_columnconfigure(0, weight=1)
+        self.master.grid_columnconfigure(1, weight=1)
+        self.master.grid_columnconfigure(2, weight=1)
+        self.master.grid_rowconfigure(0, weight=1)
 
         # Left frame with scrollbar
-        left_frame = CategoricalScrollableFrame(self.root)
+        left_frame = CategoricalScrollableFrame(self.master)
         left_frame.grid(row=0, column=0, sticky="nsew")
         left_frame.create_widgets()
 
         # Middle frame with text, entry, and button
-        middle_frame = ctk.CTkFrame(self.root)
+        middle_frame = ctk.CTkFrame(self.master)
         middle_frame.grid(row=0, column=1, sticky="nsew")
         middle_frame.grid_columnconfigure(0, weight=1)
         middle_frame.grid_rowconfigure(1, weight=1)
@@ -42,7 +39,7 @@ class MainCTKFrame:
 
         # Right frame with scrollbar
 
-        right_frame = ScrollableFrameBase(self.root,True)
+        right_frame = ScrollableFrameBase(self.master,True)
         right_frame.grid(row=0, column=2, sticky="nsew")
         right_frame.create_widgets()
         return
@@ -51,15 +48,13 @@ class MainCTKFrame:
         print("Running environment with input:")
         print(self.text_box.get("1.0", tk.END))
 
-    def run(self):
-        self.root.mainloop()
-
 
 def main():
-    mainframe=MainCTKFrame(ctk.CTk(), (800,600))
+    ctk.set_appearance_mode("dark")  # Set the theme to dark
+    CTK=ctk.CTk()
+    mainframe = MainCTKFrame(CTK, (1000, 600))
     mainframe.create_widgets()
-    mainframe.run()
-    pass
+    CTK.mainloop()
 
 
 if __name__ == "__main__":
