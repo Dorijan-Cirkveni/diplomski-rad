38608b7fb1ea104131b72d1acb890e9338aae62c
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-17 09:55:45 +0200

    Update test.py

--- display/customtkinter/test.py
+++ display/customtkinter/test.py
@@ -1,12 +1,18 @@
 import tkinter as tk
 import customtkinter as ctk
 
+
 class MainCTKFrame:
-    def __init__(self,root=ctk.):
-        self.root=C
+    def __init__(self, root: ctk.CTk, dimensions: tuple[int,int]):
+        self.root: ctk.CTk = root
+        self.root.geometry("{}x{}".format(*dimensions))
+    def run(self):
+        self.root.mainloop()
 
 
 def main():
+    mainframe=MainCTKFrame(ctk.CTk(), (800,600))
+    mainframe.run()
     pass
 
 
