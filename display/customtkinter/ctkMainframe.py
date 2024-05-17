7cfa5549d58d88adfbf93b0e46fd121f8fab681d
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-17 10:19:47 +0200

    Update ctkMainframe.py

--- display/customtkinter/ctkMainframe.py
+++ display/customtkinter/ctkMainframe.py
@@ -5,7 +5,11 @@ import customtkinter as ctk
 class MainCTKFrame:
     def __init__(self, root: ctk.CTk, dimensions: tuple[int,int]):
         self.root: ctk.CTk = root
+        ctk.set_appearance_mode("dark")
         self.root.geometry("{}x{}".format(*dimensions))
+        self.root.title("Individual Environment Test")
+        self.create_widgets(dimensions)
+
     def run(self):
         self.root.mainloop()
 
