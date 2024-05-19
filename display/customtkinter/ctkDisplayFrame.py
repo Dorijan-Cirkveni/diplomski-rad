bfbf6edd93f5eb5387c5c0f45fb14e04793c62e5
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-19 13:53:38 +0200

    Update ctkDisplayFrame.py

--- display/customtkinter/ctkDisplayFrame.py
+++ display/customtkinter/ctkDisplayFrame.py
@@ -1,6 +1,39 @@
 import ctkDisplayBase as ctkDiB
 import customtkinter as ctk
 
+class DataDisplayFrame(ctkDiB.iTkFrameDef):
+
+    def __init__(self, master, return_lambda: callable, screen_size: tuple[int, int]):
+        self.data = {
+            "winstatus": "None",
+            "error": None
+        }
+        self.order = ["winstatus", "error"]
+        self.data_label=None
+        super().__init__(master, "DataDisplay", return_lambda, screen_size)
+
+    def create_widgets(self):
+        self.data_label = ctk.CTkLabel(self, text="Inazuma shines eternal")
+        self.data_label.grid(row=0, column=0)
+        self.data_label.pack()
+
+    def display_text(self):
+        RES = []
+        S = set(self.data)
+        for e in self.order:
+            RES.append(str(self.data.get(e, "No " + e)))
+        S -= set(self.order)
+        L = list(S)
+        L.sort()
+        for e in L:
+            RES.append(str(e) + str(self.data[e]))
+        self.data_label.config(text="\n".join(RES))
+
+    def update_text(self, new_data: dict):
+        self.data.update(new_data)
+        self.display_text()
+        self.update()
+
 
 class DisplayFrame(ctkDiB.iTkFrameDef):
     def __init__(self, master, return_lambda: callable, screen_size: tuple[int, int]):
@@ -13,7 +46,8 @@ class DisplayFrame(ctkDiB.iTkFrameDef):
         right_frame.pack(side=ctk.RIGHT, fill=ctk.Y)
 
         # Frame on the bottom, 200 high, filling the remaining space
-        bottom_frame = ctk.CTkFrame(self, height=200, bg_color="blue", fg_color="blue")
+        bottom_frame = DataDisplayFrame(self,print,(self.screen_size[0],200))
+        # (self, height=200, bg_color="blue", fg_color="blue")
         bottom_frame.pack(side=ctk.BOTTOM, fill=ctk.X)
 
         # Frame in the top left, filling the remaining space
