e9fd678d12ad9e71a721bdd8e8345b06cc8b48ec
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-24 09:34:42 +0200

    Input popup working now

--- display/customtkinter/ctkPopups.py
+++ display/customtkinter/ctkPopups.py
@@ -24,25 +24,43 @@ class InputMessage(ctk.CTkToplevel):
     def __init__(self, master, title="Title", message="Message", default="", ok_text="Apply", func=print):
         super().__init__(master)
 
+        self.func = func
+
         self.title(title)
         self.geometry("300x150")
 
         self.message_label = ctk.CTkLabel(self, text=message, wraplength=250)
-        self.message_label.pack(pady=20)
+        self.message_label.pack(pady=10)
 
-        self.ok_button = ctk.CTkButton(self, text=ok_text, command=self.close)
+        self.input_entry = ctk.CTkEntry(self, width=250)
+        self.input_entry.insert(0, default)
+        self.input_entry.pack(pady=10)
+
+        self.ok_button = ctk.CTkButton(self, text=ok_text, command=self.apply_and_close)
         self.ok_button.pack(pady=10)
 
         self.grab_set()  # Make the popup modal
 
-    def close(self):
+    def apply_and_close(self):
+        input_value = self.input_entry.get()
+        self.func(input_value)
         self.destroy()
 
 
-
-
 def main():
-    return
+    root = ctk.CTk()
+    root.geometry("400x200")
+
+    def show_input_message():
+        def handle_input(value):
+            print(f"Input received: {value}")
+
+        InputMessage(root, title="Input Needed", message="Please enter some text:", func=handle_input)
+
+    test_button = ctk.CTkButton(root, text="Show Input Message", command=show_input_message)
+    test_button.pack(pady=20)
+
+    root.mainloop()
 
 
 if __name__ == "__main__":
