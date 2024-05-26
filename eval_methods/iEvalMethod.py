0e396953d6222c137e4da0851abb80e994c3f502
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-26 15:25:27 +0200

    Create iEvalMethod.py

---
+++ eval_methods/iEvalMethod.py
@@ -0,0 +1,13 @@
+class iEvalMethod:
+    def __init__(self):
+
+    def evaluate(self, data:dict, entID):
+        grid:Grid2D=
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
