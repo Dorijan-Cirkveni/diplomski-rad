3f72f9c9f369ef66c7846692b60d718462296595
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-28 16:09:02 +0200

    fixing never before seen bugs

--- agents/SimpleAgents/RuleBasedAgent.py
+++ agents/SimpleAgents/RuleBasedAgent.py
@@ -6,18 +6,22 @@ import interfaces as itf
 from util.struct.Grid2D import Grid2D
 import agents.GridAgentUtils as GAU
 
+FINAL = -1
+NONEXISTENT = -2
 
-FINAL=-1
-NONEXISTENT=-2
+
 class iRule(itf.iRawListInit):
     """
     A rule interface.
     """
 
     def __init__(self, size, startVal=tuple([])):
-        self.size=size
-        self.curvals:list[set] = [set() for i in range(size+1)]
+        self.size = size
+        self.curvals: list[set] = [set() for i in range(size + 1)]
         self.curvals[0].add(startVal)
+
+    def make_instance(self):
+        raise NotImplementedError
 
     @staticmethod
     def from_string(s):
@@ -33,18 +37,18 @@ class iRule(itf.iRawListInit):
         """
         raise NotImplementedError
 
-    def step(self, ind, values, data)->list[tuple[int,tuple]]:
+    def step(self, ind, values, data) -> list[tuple[int, tuple]]:
         raise NotImplementedError
 
-    def process(self,data):
+    def process(self, data):
         for i in range(self.size):
-            E:set=self.curvals[i]
-            self.curvals[i]=set()
+            E: set = self.curvals[i]
+            self.curvals[i] = set()
             for values in E:
-                new_values:list=self.step(i,values,data)
+                new_values: list = self.step(i, values, data)
                 while new_values:
-                    newI,newV=new_values.pop()
-                    if newI==NONEXISTENT:
+                    newI, newV = new_values.pop()
+                    if newI == NONEXISTENT:
                         continue
                     self.curvals[newI].add(newV)
         return deepcopy(self.curvals[FINAL])
@@ -54,22 +58,23 @@ class Rule(iRule):
     """
     A basic rule.
     """
-    def __init__(self, conditions: list[tuple], result):
-        self.conditions=[]
-        for e,v in conditions:
-            if type(v)==set:
-                continue
-            if type(v)==list:
-                v=set(v)
-            else:
-                v={v}
-            self.conditions.append((e,v))
-        self.result = result
-        super().__init__(len(self.conditions),self.result)
 
-    def make_instance(self,do_deepcopy=False):
-        conds=deepcopy(self.conditions) if do_deepcopy else self.conditions
-        return Rule(conds,self.result)
+    def __init__(self, conditions: list[tuple], result):
+        self.conditions = []
+        for e, v in conditions:
+            if type(v) == set:
+                continue
+            if type(v) == list:
+                v = set(v)
+            else:
+                v = {v}
+            self.conditions.append((e, v))
+        self.result = result
+        super().__init__(len(self.conditions), self.result)
+
+    def make_instance(self, do_deepcopy=False):
+        conds = deepcopy(self.conditions) if do_deepcopy else self.conditions
+        return Rule(conds, self.result)
 
     def get_keys(self):
         """
@@ -79,25 +84,35 @@ class Rule(iRule):
         return set([e[0] for e in self.conditions])
 
     def step(self, ind, values, data) -> list[tuple[int, object]]:
-        k,V=self.conditions[ind]
+        k, V = self.conditions[ind]
         if k not in data:
-            return[(ind, values)]
+            return [(ind, values)]
         if data[k] not in V:
             return [(NONEXISTENT, values)]
-        return [(ind+1, values)]
-
+        return [(ind + 1, values)]
 
 
 class iFirstOrderCondition:
-    def check(self, values, data)->[None,list[tuple[int, object]]]:
+    def check(self, values, data) -> [None, list[tuple[int, object]]]:
         raise NotImplementedError
 
 
 class FirstOrderRule(iRule):
-    def __init__(self, conditions: list[iFirstOrderCondition], result:callable, defaultValue=None):
+    def __init__(self, conditions: list[iFirstOrderCondition], resultfn=None, defaultValue=None):
+        if resultfn is None:
+            resultfn = lambda x: x
+        elif not callable(resultfn):
+            value = resultfn
+
+            def resultfn(_):
+                return value
         self.conditions = conditions
-        self.result = result
-        super().__init__(len(self.conditions),defaultValue)
+        self.resultfn = resultfn
+        self.default=defaultValue
+        super().__init__(len(self.conditions), self.default)
+
+    def make_instance(self):
+        return FirstOrderRule(self.conditions,self.curvals[0])
 
     def get_keys(self):
         """
@@ -107,35 +122,37 @@ class FirstOrderRule(iRule):
         return {"FO"}
 
     def step(self, ind, values, data) -> list[tuple[int, object]]:
-        foc:iFirstOrderCondition=self.conditions[ind]
-        new_values=foc.check(values,data)
+        foc: iFirstOrderCondition = self.conditions[ind]
+        new_values = foc.check(values, data)
         if new_values is None:
-            return [(NONEXISTENT,"NULL")]
-        return [(ind+delta,v) for delta,v in new_values]
+            return [(NONEXISTENT, "NULL")]
+        return [(ind + delta, self.resultfn(v)) for delta, v in new_values]
 
 
 class AscendingTestVariableCondition(iFirstOrderCondition):
     def __init__(self, maxval):
         self.maxval = maxval
 
-    def check(self, value:tuple, data):
-        if len(value)==0:
-            return [(1,(e,)) for e in data]
-        test=[]
-        if len(data)<self.maxval+1:
-            test=[e for e in data if value[-1]<e]
+    def check(self, value: tuple, data):
+        if len(value) == 0:
+            return [(1, (e,)) for e in data]
+        test = []
+        if len(data) < self.maxval + 1:
+            test = [e for e in data if value[-1] < e]
         else:
-            test=[i for i in range(value[-1]+1,self.maxval+1) if i in data]
-        return [(1,e) for e in test]
+            test = [i for i in range(value[-1] + 1, self.maxval + 1) if i in data]
+        return [(1, e) for e in test]
+
 
 class RulesetManager:
-    def __init__(self, rules:list, byElement=None):
+    def __init__(self, rules: list, byElement=None):
         self.rules = []
-        self.byElement=defaultdict(set)
+        self.byElement = defaultdict(set)
         if byElement is not None:
-            self.rules=rules
-            self.byElement=byElement
-        for rule in self.rules:
+            self.rules = rules
+            self.byElement = byElement
+            return
+        for rule in rules:
             self.add(rule)
 
     def add(self, rule: iRule):
@@ -145,18 +162,17 @@ class RulesetManager:
         for cat in X:
             self.byElement[cat].add(ruleID)
 
-
     def make_instance(self):
         rules = [rule.make_instance() for rule in self.rules]
-        return RulesetManager(rules,deepcopy(self.byElement))
+        return RulesetManager(rules, deepcopy(self.byElement))
 
     def process_current(self, data: dict):
-        new_data=set()
+        new_data = set()
         for rule in self.rules:
-            results=rule.process(data)
+            results = rule.process(data)
             print(results)
-            new_data|=results
-        return
+            new_data |= results
+        return new_data
 
 
 class RuleBasedAgent(itf.iAgent):
@@ -166,43 +182,40 @@ class RuleBasedAgent(itf.iAgent):
 
     fullname = "Rule Based Agent"
     DEFAULT_STR_INPUT = None
-    DEFAULT_RAW_INPUT = [[],{'rel':Grid2D((3,3),[[0,1,0],[1,1,1],[0,1,0]])},]
+    DEFAULT_RAW_INPUT = [[], {'rel': Grid2D((3, 3), [[0, 1, 0], [1, 1, 1], [0, 1, 0]])}, ]
     INPUT_PRESETS = {}
+
     def __init__(self, rulelist: list, used: dict, pers_vars: set = None, defaultAction=ACTIONS[-1]):
         super().__init__()
         self.manager = RulesetManager()
-        self.states=[]
+        self.states = []
         for rule in rulelist:
             self.manager.add(rule)
         self.used = used
         self.pers_vars: set = {} if pers_vars is None else pers_vars
         self.defaultAction = defaultAction
 
-
-
     def receiveEnvironmentData(self, data: dict):
-        self.memory.step_iteration({"grid","agents","persistent"},False)
+        self.memory.step_iteration({"grid", "agents", "persistent"}, False)
         curManager: RulesetManager = self.manager.make_instance()
-        ret={}
-
-
+        ret = {}
 
     def performAction(self, actions):
-        action = self.memory.get_data([("action",self.defaultAction)])
+        action = self.memory.get_data([("action", self.defaultAction)])
         return action
+
 
 def ruleTest():
     rule = AscendingTestVariableCondition(999)
-    example= {
+    example = {
         1: True,
         2: True,
         3: None
     }
-    LX = [('A1', True), ('A2', True), ('A3', True)]
-    R1 = FirstOrderRule([rule],('A',True),tuple([]))
-    print(">",R1.step(0,(-1,),example))
-    print(">",R1.step(0,(11,),example))
-    print(">",R1.process(example))
+    R1 = FirstOrderRule([rule], ('A', True), tuple([]))
+    print(">", R1.step(0, (-1,), example))
+    print(">", R1.step(0, (11,), example))
+    print(">", R1.process(example))
 
 
 def main():
--- agents/unittests/RBA_unittest.py
+++ agents/unittests/RBA_unittest.py
@@ -25,7 +25,7 @@ class TestFirstOrderRule(unittest.TestCase):
     def test_first_order_rule_processing(self):
         rule = AscendingTestVariableCondition(999)
         example = {i: True for i in [2,3,7,9,11,13,15,17]}
-        R1 = FirstOrderRule([rule], ('A', True), tuple([]))
+        R1 = FirstOrderRule([rule], None, tuple([]))
 
         step_result_1 = R1.step(0, (-1,), example)
         self.assertEqual(step_result_1, [(1, e) for e in example])
@@ -36,6 +36,95 @@ class TestFirstOrderRule(unittest.TestCase):
 
         process_result = R1.process(example)
         self.assertIsInstance(process_result, set)
+
+class TestRulesetManager(unittest.TestCase):
+    def setUp(self):
+        """
+        Set up initial conditions for each test.
+        """
+        # Create a few rules to be used in the tests
+        rule1_conditions = [('A1', True), ('A2', True), ('A3', True)]
+        self.rule1 = Rule(rule1_conditions, ('A', True))
+
+        rule2_conditions = [AscendingTestVariableCondition(999)]
+        self.rule2 = FirstOrderRule(rule2_conditions, lambda x: ('B', True), tuple([]))
+
+        self.rules = [self.rule1, self.rule2]
+        self.manager = RulesetManager(self.rules)
+        assert len(self.manager.rules)==2
+        assert self.manager.rules[1] is self.rule2
+
+    def test_add_rule(self):
+        """
+        Test adding a rule to the manager.
+        """
+        rule_conditions = [('B1', True)]
+        new_rule = Rule(rule_conditions, ('B', True))
+        self.manager.add(new_rule)
+
+        self.assertIn(new_rule, self.manager.rules)
+        self.assertIn('B1', self.manager.byElement)
+
+    def test_make_instance(self):
+        """
+        Test making an instance of the ruleset manager.
+        """
+        new_manager = self.manager.make_instance()
+
+        self.assertIsInstance(new_manager, RulesetManager)
+        self.assertEqual(len(new_manager.rules), len(self.manager.rules))
+        self.assertNotEqual(id(new_manager.rules), id(self.manager.rules))
+
+    def test_process_current(self):
+        """
+        Test processing current data with the ruleset manager.
+        """
+        data = {
+            'A1': True,
+            'A2': True,
+            'A3': True,
+            1: True,
+            2: True
+        }
+        data1=self.rule1.process(data)
+        self.assertEqual(data1,{('A',True)})
+        results = self.manager.process_current(data)
+        expected_results = {('A', True), ('B', True)}
+
+        self.assertEqual(results, expected_results)
+
+    def test_process_rule1(self):
+        """
+        Test processing a specific rule.
+        """
+        data = {
+            'A1': True,
+            'A2': True
+        }
+
+        self.assertEqual(self.rule1.process(data), set())
+
+        data['A3'] = True
+        result = self.rule1.process(data)
+        self.assertEqual(result, {('A', True)})
+
+    def test_process_rule2(self):
+        """
+        Test processing a specific first-order rule.
+        """
+        data = {
+            1: True,
+            2: True,
+            3: None
+        }
+
+        result = self.rule2.process(data)
+        self.assertIsInstance(result, set)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 
 if __name__ == '__main__':
