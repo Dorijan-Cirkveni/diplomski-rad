73db740cba96f2101e979536771c4c4d2741d0ba
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-05-29 12:15:12 +0200

    Update AgentDataPreprocessor.py

--- agents/AgentUtils/AgentDataPreprocessor.py
+++ agents/AgentUtils/AgentDataPreprocessor.py
@@ -14,13 +14,14 @@ class AgentDataPreprocessor:
         for comp in self.components:
             new_data = comp.processAgentData(data)
             data = new_data
-        return
+        return data
 
 class ReLocADP(iADPComponent):
     def processAgentData(self, data: dict) -> dict:
         if "loc" not in data:
             data["error"]="no location"
             return data
+        loc=data["loc"]
         if "grid" not in data:
             data["error"]="no grid"
             return data
@@ -28,6 +29,8 @@ class ReLocADP(iADPComponent):
         if not isinstance(grid,Grid2D):
             data["error"]="invalid grid"
             return data
+        grid.get_relpos(loc,keymaker=lambda x:('rel',x),retdict=data)
+        return data
 
 
 
