ea2e2bf6987668881076336836919a8867297490
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-06-05 12:03:52 +0200

    added unit test

--- agents/unittests/RBA_unittest.py
+++ agents/unittests/RBA_unittest.py
@@ -103,5 +103,75 @@ class TestRulesetManager(unittest.TestCase):
         self.assertIsInstance(result, dict)
 
 
+class TestRulesetManagerAgain(unittest.TestCase):
+    def setUp(self):
+        # Create a few rules to be used in the tests
+        rule1_conditions = [('A1', True), ('A2', True), ('A3', True)]
+        self.rule1 = PropRule(rule1_conditions, ('A', True))
+
+        rule2_exit=SimpleZeroCondition(RLiteral('B', True))
+        rule2_conditions = [AscendingTestVariableCondition(999), rule2_exit]
+        self.rule2 = FirstOrderRule(rule2_conditions,[tuple([])])
+
+        self.rules = [self.rule1, self.rule2]
+        self.manager = RulesetManager(self.rules)
+        assert len(self.manager.rules) == 2
+        assert self.manager.rules[1] is self.rule2
+
+    def test_add_rule(self):
+        rule_conditions = [('B1', True)]
+        new_rule = PropRule(rule_conditions, ('B', True))
+        self.manager.add(new_rule)
+
+        self.assertIn(new_rule, self.manager.rules)
+        self.assertIn('B1', self.manager.byElement)
+
+    def test_make_instance(self):
+        new_manager = self.manager.make_instance()
+
+        self.assertIsInstance(new_manager, RulesetManager)
+        self.assertEqual(len(new_manager.rules), len(self.manager.rules))
+        self.assertNotEqual(id(new_manager.rules), id(self.manager.rules))
+
+    def test_process_current(self):
+        data = {
+            'A1': True,
+            'A2': True,
+            'A3': True,
+            1: True,
+            2: True
+        }
+        data1 = self.rule1.process(data, set(data))
+        self.assertEqual(data1, {RLiteral('A', True):True})
+        results = self.manager.process_current(data, set(data))
+        expected_results = {'A': True, 'B': True}
+
+        self.assertEqual(results, expected_results)
+
+    def test_process_rule1(self):
+        data = {
+            'A1': True,
+            'A2': True
+        }
+
+        self.assertEqual(self.rule1.process(data, set(data)), dict())
+
+        data['A3'] = True
+        result = self.rule1.process(data, set(data))
+        self.assertEqual(result, {RLiteral('A', True):True})
+
+    def test_process_rule2(self):
+        data = {
+            1: True,
+            2: True,
+            3: None
+        }
+
+        result = self.rule2.process(data)
+        self.assertIsInstance(result, dict)
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
