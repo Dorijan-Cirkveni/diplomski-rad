9c1dc05c8fc26f676758e386fe99a8a209ed94a4
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-06-07 14:20:46 +0200

    RPM

--- environments/RavenProgressiveMatrixTest.py
+++ environments/RavenProgressiveMatrixTest.py
@@ -11,28 +11,31 @@ def RectGrid(s,minscale:tuple[int,int], translator:callable=int):
     return Grid2D(minscale,M=M,default=0)
 
 
+def RPMGrid(known_tiles:list[Grid2D], sol_tile:Grid2D, wrong_tiles:Grid2D, tilescale:tuple=(3,3)):
+    preset={
+            "scale": [25,25],
+            "shapes": {
+              "rect": [
+                [0, 18, 19, 19, 1],
+                [0, 0, 24, 24, 2],
+                [6, 2, 10, 4, 2]
+              ]
+            }
+          }
+    solid=Grid2D.raw_init(preset)
+
+
+
 class RavenProgressiveMatrixTest(GridEnvironment):
 
     def __init__(self, known_tiles:list[str], sol_tile:str, wrong_tiles:list[str], tilescale:tuple=(3,3)):
-        preset={
-                "scale": [25,25],
-                "shapes": {
-                  "chkb": [
-                    [[8,8],[12,12],[0,4]]
-                  ],
-                  "rect": [
-                    [0, 18, 19, 19, 1],
-                    [0, 0, 19, 19, 2],
-                    [6, 2, 10, 4, 2]
-                  ]
-                }
-              }
         solid=Grid2D.raw_init(preset)
         ktc=tilescale[0]*tilescale[1]-1
         rtc=len(wrong_tiles)+1
         assert len(known_tiles)==ktc
         KG=[RectGrid(e) for e in known_tiles]
-        ST=[]
+        solG=RectGrid(sol_tile)
+        WT=[]
         gridRoutines={
             "solid":
         }
