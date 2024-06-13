de12aafde59b879555e7e7fb5ec64b66d0e58280
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2024-06-13 12:52:02 +0200

    rawimporter

--- util/struct/baseClasses.py
+++ util/struct/baseClasses.py
@@ -9,12 +9,12 @@ class iRawInit:
     """
 
     @classmethod
-    def from_string(cls,s):
+    def from_string(cls, s):
         """
 
         :param s:
         """
-        raw=json.loads(s)
+        raw = json.loads(s)
         return cls.raw_init(raw)
 
     @classmethod
@@ -23,8 +23,8 @@ class iRawInit:
 
         :param raw:
         """
-        params=inspect.signature(cls.__init__).parameters
-        parkeys=[(e,params[e].default) for e in list(params)[1:]]
+        params = inspect.signature(cls.__init__).parameters
+        parkeys = [(e, params[e].default) for e in list(params)[1:]]
         if type(raw) == dict:
             pro_d: dict
             pro_d = cls.raw_process_dict(raw, parkeys)
@@ -39,39 +39,38 @@ class iRawInit:
             result = cls(*pro_l)
         else:
             raise NotImplementedError
-        result:iRawDictInit
+        result: iRawDictInit
         result.raw_post_init()
         return result
-
 
     def raw_post_init(self):
         return
 
     @staticmethod
-    def raw_process_dict(raw: dict, params:list):
+    def raw_process_dict(raw: dict, params: list):
         """
 
         :param raw:
         :param params:
         :return:
         """
-        return {e:raw.get(e,v) for e,v in params}
+        return {e: raw.get(e, v) for e, v in params}
 
     @staticmethod
-    def raw_process_list(raw: list, params:list) -> list:
+    def raw_process_list(raw: list, params: list) -> list:
         """
 
         :param raw:
         :param params:
         :return:
         """
-        n=len(raw)
-        if n<len(params) and params[n][1]==inspect.Parameter.empty:
-            X=[e for e in params if e[1]==inspect.Parameter.empty]
-            raise Exception("Not enough parameters ({}/{})!".format(n,len(X)))
-        X=[e[1] for e in params]
+        n = len(raw)
+        if n < len(params) and params[n][1] == inspect.Parameter.empty:
+            X = [e for e in params if e[1] == inspect.Parameter.empty]
+            raise Exception("Not enough parameters ({}/{})!".format(n, len(X)))
+        X = [e[1] for e in params]
         for i in range(n):
-            X[i]=raw[i]
+            X[i] = raw[i]
         return X
 
     def __copy__(self):
@@ -102,8 +101,9 @@ class iRawDictInit(iRawInit):
     """
     Interface exclusive to dictionaries.
     """
+
     @staticmethod
-    def raw_process_list(raw: list, params:list) -> list:
+    def raw_process_list(raw: list, params: list) -> list:
         """
 
         :param raw:
@@ -115,13 +115,45 @@ class iRawListInit(iRawInit):
     """
     Interface exclusive to lists.
     """
+
     @staticmethod
-    def raw_process_dict(raw: dict, params:list):
+    def raw_process_dict(raw: dict, params: list):
         """
 
         :param raw:
         """
         raise Exception("Must be list, not dictionary!")
+
+
+class RawImporter:
+    main = None
+
+    def __init__(self):
+        self.dict_patterns: list[tuple[callable, iRawInit]] = []
+        self.list_patterns: list[tuple[callable, iRawInit]] = []
+
+    @classmethod
+    def GetMain(cls):
+        if cls.main is None:
+            cls.main = RawImporter
+        return cls.main
+
+    def add_pattern(self, pattern: tuple, isDict: bool, isList: bool):
+        if isDict:
+            self.dict_patterns.append(pattern)
+        if isList:
+            self.list_patterns.append(pattern)
+
+    def raw_init(self, raw_data):
+        if isinstance(raw_data, dict):
+            patterns = self.dict_patterns
+        elif isinstance(raw_data, list):
+            patterns = self.list_patterns
+        else:
+            return None
+        for key, value in patterns:
+            if key(raw_data):
+                return value.raw_init(raw_data)
 
 
 def main():
